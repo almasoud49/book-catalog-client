9a939bd68c68428eedcb0ed4cf01ed07ebfdd868
Abdullah Al Masoud <almasoud49@yahoo.com>
2023-08-20 15:10:21 +0600

    [DONE] Navbar Part

---
+++ src/layout/Main.tsx
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
+
+
+
+const Main = ()=> {
+    return (
+        <div>
+          <Navbar />
+          <div className="pt-16  mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <Outlet />
+          </div>
+        </div>
+      );
+
+};
+
+export default Main;
---
+++ src/layout/Navbar.tsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+import { logout } from "../redux/features/users/userSlice";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+
+const Navbar = () => {
+const {accessToken} = useAppSelector((state)=> state.user);
+
+const dispatch = useAppDispatch();
+
+const handleLogout = () => {
+    dispatch(logout)
+}
+
+
+return (
+    <nav className="bg-gray-800">
+      <div className="container mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex">
+            
+            <Link
+              to="/"
+              className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Home
+            </Link>
+            {accessToken && (
+              <Link
+                to="/profile"
+                className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Profile
+              </Link>
+            )}
+          </div>
+          <div className="flex">
+            {!accessToken && (
+              <>
+                <Link
+                  to="/signup"
+                  className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Sign Up
+                </Link>
+                <Link
+                  to="/login"
+                  className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Login
+                </Link>
+              </>
+            )}
+            {accessToken && (
+              <span
+                onClick={handleLogout}
+                className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Logout
+              </span>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
